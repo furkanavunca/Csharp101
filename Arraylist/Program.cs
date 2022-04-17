e76bdfb9b1826ec60e91006bbada2f094520fa2b
FBA <fbatuhanavunca@gmail.com>
2022-04-17 13:21:06 +0300

    Arraylist

---
+++ Arraylist/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            //liste.Add("Furkan");
+            //liste.Add(22);
+            //liste.Add(true);
+            //liste.Add('A');
+
+            // İçerisindeki verilere erişme
+
+            //Console.WriteLine(liste[1]);
+            //foreach (var item in liste)
+                //Console.WriteLine(item);
+
+            //AddRange
+            Console.WriteLine("***** Add Range *****");
+            //string[] renkler = {"Kırmızı","Sarı","Yeşil"};
+            List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+            //liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+            
+            //Sort
+            Console.WriteLine("***** Sort *****");
+            liste.Sort();
+
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            // Binary Search
+            Console.WriteLine("***** Binary Search *****");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            // Reverse
+            Console.WriteLine("***** Reverse *****");
+            liste.Reverse();
+            foreach (var item in liste)
+                Console.WriteLine(item);
+
+            //Clear
+            Console.WriteLine("***** Clear *****");
+            liste.Clear();
+            foreach (var item in liste)
+                Console.WriteLine(item);
+        }
+    }
+}
